d8788d7a039486d2f53cd5231b48b825abb6ff0e
abdalla-dev453 <abdallamsema69@gmail.com>
2026-08-25 15:15:46 +0300

    fixed the app factory

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
-from app import create_app
+from app import create_app, db 
 
 app = create_app(os.environ.get("FLASK_ENV", "development"))
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
